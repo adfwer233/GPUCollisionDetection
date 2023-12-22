5725e117ef9d8465cad528238a8af179b96f4db0
Bao Anchang <ba123698745@126.com>
2023-12-22 23:57:38 +0800

    Update fetch-gtest.cmake

--- cmake/fetch-gtest.cmake
+++ cmake/fetch-gtest.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 
 FetchContent_Declare(
         gtest
-        URL https://github.com/google/googletest/archive/03597a01ee50ed33e9dfd640b249b4be3799d395.zip
+        URL https://github.com/google/googletest/archive/f8d7d77c06936315286eb55f8de22cd23c188571.zip
 )
 
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
